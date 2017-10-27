1e5070bb7577f84d2b79b8c4a87c05c8e227dae3
Tawsif Ahmed <ahmedt93@gmail.com>
2017-10-27 11:44:15 -0400

    added code for percent scrollable

--- inview_percent_scrollable.js
+++ inview_percent_scrollable.js
@@ -1,42 +1,34 @@
 window.getInviewPercent = function() {
-    setInterval(() => {
 
-        // Get the target ad node
-        let target = document.querySelector('#sidebar img');
-        // Get the node's bounding client rect and extract the needed properties
-        let boundingRect = target.getBoundingClientRect();
-        let { x, y, width, height } = boundingRect;
-        console.log(y);
+    // Get the target ad node
+    let target = document.querySelector('#sidebar img');
+    // Get the node's bounding client rect and extract the needed properties
+    let boundingRect = target.getBoundingClientRect();
+    let { x, y, width, height } = boundingRect;
 
-        // If x/left coordinate is less than 0, add to the width of the target
-        // Otherwise, return the width of the target
-        let currentWidth = x < 0 ? width + x : width;
+    // If x/left coordinate is less than 0, add to the width of the target
+    // Otherwise, return the width of the target
+    let currentWidth = x < 0 ? width + x : width;
 
-        // If y/top coordinate is less than 0, add to the height of the target
-        // Otherwise, subtract the coordinate from the viewport's height.
-        let currentHeight = y < 0 ? height + y : window.innerHeight - y;
+    // If y/top coordinate is less than 0, add to the height of the target
+    // Otherwise, subtract the coordinate from the viewport's height.
+    let currentHeight = y < 0 ? height + y : window.innerHeight - y;
 
-        // If currentHeight is negative, it is not in the viewport.
-        // Else, if calculated currentheight is greater than target height, set to its height
-        if (currentHeight < 0) {
-            currentHeight = 0;
-        } else if (currentHeight > height) {
-            currentHeight = height;
-        }
+    // If currentHeight is negative, it is not in the viewport.
+    // Else, if calculated currentheight is greater than target height, set to its height
+    if (currentHeight < 0) {
+        currentHeight = 0;
+    } else if (currentHeight > height) {
+        currentHeight = height;
+    }
 
-        // Calculate the total area of the target
-        let totalArea = width * height;
-        // Calculate the visible area of the target
-        let currentArea = currentWidth * currentHeight;
-        // Calculate the percentage of the visible area
-        let percent =  (currentArea / totalArea) * 100;
+    // Calculate the total area of the target
+    let totalArea = width * height;
+    // Calculate the visible area of the target
+    let currentArea = currentWidth * currentHeight;
+    // Calculate the percentage of the visible area
+    let percent =  (currentArea / totalArea) * 100;
 
-        console.log(y, innerHeight, currentHeight, percent);
-
-        // Dynamically update the percentage on the DOM
-        let answerSpan = document.getElementById('answer');
-        answerSpan.innerText = percent;
-
-    }, 2000)
+    return percent;
 
 };
